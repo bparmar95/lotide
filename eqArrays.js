8b272d9356f5eeb6668196d3e808e0461fd3796b
Baltej Parmar <bparmar@uwaterloo.ca>
2020-07-22 00:19:51 +0000

    update assert equal to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
+  val = ""
+  if (arr1.length === arr2.length) {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        val = true;
+      } else {
+        return false;
+      }
+    }
+    return val;
+  }
+  return false;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),true) // => should fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false) // => pass
