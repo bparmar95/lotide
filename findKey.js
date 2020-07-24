686b380ffb6857dd10a958fd6ce6eb1d723c446b
Baltej Parmar <bparmar@uwaterloo.ca>
2020-07-24 04:04:43 +0000

    Find key implementation

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (let i in obj) {
+    if ((callback(obj[i]))) {
+      return i;
+    }
+  }
+}
+
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4) // => undefined
+
+assertEqual(result, "noma");
+assertEqual(result, "Ora");
+assertEqual(result2, undefined);
+assertEqual(result, "Blue Hill");
